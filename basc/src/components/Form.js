c429c033f8aeace8754585f607a59dfebc607661
Rober Andujar <randujar@ZPERTA.local>
2022-10-19 15:28:23 -0400

    adding components folder

---
+++ basc/src/components/Form.js
@@ -0,0 +1,28 @@
+import React from 'react'
+
+function Form() {
+  return (
+    <div>
+        <form>
+            <h3>Razon Social</h3>
+            <input type="text"></input>
+            <h3>RNC</h3>
+            <input type="text"></input>
+            <h3>Dirección</h3>
+            <input type="text"></input>
+            <h3>Ciudad</h3>
+            <input type="text"></input>
+            <h3>Provincia</h3>
+            <input type="text"></input>
+            <h3>País</h3>
+            <input type="text"></input>
+            <h3>Teléfono</h3>
+            <input type="text"></input>
+            <h3>Email</h3>
+            <input type="email"></input>
+        </form>
+    </div>
+  )
+}
+
+export default Form 
